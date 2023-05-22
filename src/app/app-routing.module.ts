c492febba55166bb1fc5f123948ce801575d60ac
Hiren <rebeldevmurari@gmail.com>
2023-05-22 23:03:57 +0100

    Login form is ready, navbar is ready, and also implemented some designing css and navigation links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SingupComponent } from './pages/singup/singup.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
@@ -14,9 +15,14 @@ const routes: Routes = [
 {
   path: 'signup',
   component: SingupComponent,
-  pathMatch: 'full'
- 
+  pathMatch: 'full' 
   
+},
+
+{
+  path: 'login',
+  component: LoginComponent,
+  pathMatch: 'full'
 }
 
 
