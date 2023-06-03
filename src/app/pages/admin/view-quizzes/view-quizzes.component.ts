4f918c0727acfd161d19a358c97800792a505048
Hiren <rebeldevmurari@gmail.com>
2023-06-03 14:12:18 +0100

    added some frond end and backends finctions foe the Quiz Model, added, view-quizzes and add-quiz functions, tested with backed successfully.

--- src/app/pages/admin/view-quizzes/view-quizzes.component.ts
+++ src/app/pages/admin/view-quizzes/view-quizzes.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-quizzes',
   templateUrl: './view-quizzes.component.html',
   styleUrls: ['./view-quizzes.component.css']
 })
-export class ViewQuizzesComponent {
+export class ViewQuizzesComponent implements OnInit {
+
+  quizzes=[
+    {
+      qId:'',
+      title: '',
+      description: '',
+      maxMarks: '',
+      numberOfQuestions: '',
+      category:{
+        cId:'',
+        title:'',
+        description: '',
+      },
+
+    },
+    
+
+  ];
+
+  constructor(private quizSer:QuizService){}
+
+  ngOnInit(): void {
+      
+    //getting list of Quzzies
+    this.quizSer.getListQuizzes().subscribe({
+      next: (data:any)=>
+      {
+        this.quizzes = data;
+
+      },
+      error : (error)=>
+      {
+        console.log(error);
+        Swal.fire('Error', 'Something went wrong !! ', 'error');
+      }  
+
+    });
+
+
+  }
+
 
 }
---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor(private http:HttpClient) { }
+
+  //getting list of quizzes
+  public getListQuizzes()
+  {
+    return this.http.get(`${baseUrl}/api/v1/quiz/`);
+  }
+
+  //Adding a new quiz
+  public addNewQuiz(quiz:any)
+  {
+    return this.http.post(`${baseUrl}/api/v1/quiz/` ,quiz);
+  }
+}
